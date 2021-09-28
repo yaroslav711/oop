043b313f7b6743b114245b948ee5faa3a267de38
Yaroslav <65777152+yaroslav711@users.noreply.github.com>
2021-09-28 18:28:32 +0300

    Add files via upload

--- Header.cpp
+++ Header.cpp
@@ -1,13 +1,19 @@
 #include "Header.h"
+
+//конструктор по умолчанию
 Ingredient::Ingredient() {
     name = "";
     unit = "";
     count = 0;
 }
 
+// контруктор копирования
 Ingredient::Ingredient(const Ingredient& ingredient) {
     name = ingredient.name;
     unit = ingredient.unit;
     count = ingredient.count;
 }
 
+//деструктор
+Ingredient::~Ingredient(){
+}
